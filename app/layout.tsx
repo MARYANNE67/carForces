99992bd7e1ed259a132b87d242d2c070174cd3d6
MARYANNE67 <93769788+MARYANNE67@users.noreply.github.com>
2024-05-11 01:41:12 -0400

    updated readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { relative } from "path";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
-  title: "Car Force",
+  title: "Car Forces",
   description: "Explore great rental cars",
 };
 
